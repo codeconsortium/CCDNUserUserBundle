d71dd15aaa9a3c39ad2e7e6a59b32121c6a8a65e
Reece Fowell <reece@codeconsortium.com>
2012-11-11 18:48:51 +0000

    Add subscriber for route referer compiller pass for ignore list.

---
+++ Component/RouteRefererIgnore/RouteRefererIgnoreList.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the CCDNUser UserBundle
+ *
+ * (c) CCDN (c) CodeConsortium <http://www.codeconsortium.com/>
+ *
+ * Available on github <http://www.github.com/codeconsortium/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CCDNUser\UserBundle\Component\RouteRefererIgnore;
+
+/**
+ *
+ * @author Reece Fowell <reece@codeconsortium.com>
+ * @version 1.0
+ */
+class RouteRefererIgnoreList
+{
+
+    /**
+     *
+ 	 * @access public
+	 * @return mixed[]
+     */
+    public function getRoutes()
+    {
+		$ignore = array(
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_security_login'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_security_check'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_security_logout'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_registration_register'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_registration_check_email'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_registration_confirm'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_registration_confirmed'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_resetting_request'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_resetting_send_email'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_resetting_check_email'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_resetting_reset'),
+			array('bundle' => 'fosuserbundle', 'route' => 'fos_user_change_password'),
+			array('bundle' => 'ccdnuseruserbundle', 'route' => 'ccdn_user_user_registration_terms'),
+		);
+		
+		return $ignore;
+    }
+
+}
